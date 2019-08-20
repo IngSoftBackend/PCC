c5aa966cccaf88adb01e93350003869dcf4f37a1
Cristhian Laurente <frontend.crstn@gmail.com>
2019-08-19 22:45:42 -0500

    Cree carpetas Frontend para lo que son img, css, js, html y agregue mi CV-Portafolio

---
+++ app/js/frontend/ctrl.cv.cristhianlaurente.js
@@ -0,0 +1,55 @@
+const grid = new Muuri(".grid", {
+    layout: {
+        rounding: false
+      }
+});
+
+window.addEventListener("load", () => {
+    grid.refreshItems().layout();
+    document.getElementById("grid").classList.add("imagenes-cargadas");
+
+    //Agregamos los Listener de los enlaces para filtrar por categoria//
+
+    const enlaces = document.querySelectorAll("#categorias a");
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener("click", (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove("activo"));
+            evento.target.classList.add('activo');
+
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === "todos" ? grid.filter("[data-categoria]") : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+
+    //Agregamos el Listener para la barra de Busqueda//
+    document.querySelector("#barra-busqueda").addEventListener("input", (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+    });
+
+
+    //Agregamos Listener para las Imagenes//
+    const overlay = document.getElementById("overlay");
+    document.querySelectorAll(".grid .item img").forEach((elemento) => {
+        elemento.addEventListener("click", () => {
+            const ruta = elemento.getAttribute("src");
+            const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+            
+            overlay.classList.add("activo");
+            document.querySelector("#overlay img").src = ruta;
+            document.querySelector("#overlay .descripcion").innerHTML = descripcion;
+        });
+    });
+
+    //EventListener del boton de Cerrar//
+    document.querySelector("#btn-cerrar-popup").addEventListener("click", () => {
+        overlay.classList.remove("activo");
+    });
+
+    // EventListener del overlay//
+    overlay.addEventListener("click", (evento) => {
+        evento.target.id === "overlay" ? overlay.classList.remove("activo") : "";
+    });
+});
